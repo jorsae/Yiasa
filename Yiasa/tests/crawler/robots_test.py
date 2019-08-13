7024ee5193f65598a0ad9bac7d47c0e63084df88
jorsae <jorgen.sater@gmail.com>
2019-08-13 17:28:10 +1000

    Added empty case for test_parse_robots

--- Yiasa/tests/crawler/robots_test.py
+++ Yiasa/tests/crawler/robots_test.py
@@ -1,4 +1,5 @@
 import pytest
+import requests
 import sys
 sys.path.append("web/")
 sys.path.append("crawler/")
@@ -8,12 +9,16 @@ import robot
 
 testdata_test_parse_robots = [
     ('https://www.vg.no/robots.txt', {'Allow':[], 'Disallow':['/tegneserier/salesposter']}),
-    ('https://www.reddit.com/robots.txt', {'Allow':['/partner_api/', '/', '/sitemaps/*.xml'], 'Disallow':['/*.json', '/*.json-compact', '/*.json-html', '/*.xml', '/*.rss', '/*.i', '/*.embed', '/*/comments/*?*sort=', '/r/*/comments/*/*/c*', '/comments/*/*/c*', '/r/*/submit$', '/r/*/submit/$', '/message/compose*', '/api', '/post', '/submit', '/goto', '/*after=', '/*before=', '/domain/*t=', '/login', '/r/*/user/', '/gold?']})
+    ('https://www.reddit.com/robots.txt', {'Allow':['/partner_api/', '/', '/sitemaps/*.xml'], 'Disallow':['/*.json', '/*.json-compact', '/*.json-html', '/*.xml', '/*.rss', '/*.i', '/*.embed', '/*/comments/*?*sort=', '/r/*/comments/*/*/c*', '/comments/*/*/c*', '/r/*/submit$', '/r/*/submit/$', '/message/compose*', '/api', '/post', '/submit', '/goto', '/*after=', '/*before=', '/domain/*t=', '/login', '/r/*/user/', '/gold?']}),
+    ('', {'Allow':[], 'Disallow':[]})
 ]
 
 @pytest.mark.parametrize("url,expected", testdata_test_parse_robots)
 def test_parse_robots(url, expected):
-    req = request.get_request(url)
     robots = robot.Robots()
-    robots.parse_robots(req.text)
+    try:
+        req = request.get_request(url)
+        robots.parse_robots(req.text)
+    except:
+        pass
     assert(robots.rules) == expected
