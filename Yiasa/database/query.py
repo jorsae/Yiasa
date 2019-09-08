6a3b72f66eab865742ca218e96eca15c62c8a949
jorsae <jorgen.sater@gmail.com>
2019-09-08 18:29:21 +1000

    Cleaned up comments in query.py

--- Yiasa/database/query.py
+++ Yiasa/database/query.py
@@ -1,14 +1,17 @@
-""" Database TABLE NAMES """
-
+# Database TABLE NAMES
 TABLE_DOMAIN = 'domain'
 TABLE_CRAWL_HISTORY = 'crawl_history'
 TABLE_CRAWL_QUEUE = 'crawl_queue'
 TABLE_EMAILS = 'emails'
 
+# Generel queries
 table_exists = """SELECT name from sqlite_master WHERE type='table' and name=? """
 
+# Insert queries
 insert_table_domain = f'INSERT INTO {TABLE_DOMAIN} VALUES (?, ?, ?)'
+insert_table_crawl_queue = f'INSERT INTO {TABLE_CRAWL_QUEUE} VALUES (?, ?, ?)'
 
+# Create queries
 create_table_domain = f"""CREATE TABLE {TABLE_DOMAIN} (
                 scheme text,
                 amount_crawled int,
