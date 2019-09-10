efc7d420192b1986a4159372861ad55178e24c4b
jorsae <jorgen.sater@gmail.com>
2019-09-11 04:24:44 +1000

    Added simple test for multi threading in program.py

--- Yiasa/program.py
+++ Yiasa/program.py
@@ -1,5 +1,7 @@
 import requests
 import sys
+import threading
+import time
 sys.path.append('crawler/')
 sys.path.append('utility/')
 sys.path.append('web/')
@@ -19,9 +21,36 @@ def main():
     utility.setup_logging()
     dump()
     test_full_fld_crawl()
+    #multi_thread()
+
+def multi_thread():
+    t1 = threading.Thread(target=test_full_fld_crawl)
+    t1.start()
+    t2 = threading.Thread(target=test_full_fld_crawl2)
+    t2.start()
+
+    while True:
+        print('lalala')
+        print(f't1: {t1.is_alive()}')
+        print(f't2: {t2.is_alive()}')
+        time.sleep(1)
 
 def dump():
     p.database.dump_database()
+
+def test_full_fld_crawl2():
+    url = 'google.com'
+    fld = utility.get_fld(url)
+    setup_db = p.database.setup_database()
+    spider = crawler.Crawler(fld, p)
+    spider.extractor.robots.rules["Disallow"].append("\S+/Partier/\S+")
+    spider.extractor.robots.rules["Disallow"].append("/\S+.html")
+    spider.extractor.robots.rules["Disallow"].append("/javascript")
+    spider.extractor.robots.rules["Disallow"].append("\S+.cbv")
+    spider.extractor.robots.rules["Disallow"].append("\S+2014")
+    spider.extractor.robots.rules["Disallow"].append("\S+beta")
+    print('Starting crawling')
+    spider.start_crawling()
 
 def test_full_fld_crawl():
     """ This is just a short website, that my bot can crawl through it's entirety fast,
